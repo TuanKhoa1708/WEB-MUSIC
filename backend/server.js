32faa1f5753c3b8bfa780c9afc04d091688c7ab2
hoabuiquang168-star <hoabuiquang168@gmail.com>
2026-07-28 00:07:58 +0700

    UpdateLogin\Register

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
+console.log("MONGODB_URI =", process.env.MONGODB_URI);
+
+// Kết nối MongoDB
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Backend is running 🚀",
+    });
+});
+
+// Auth Routes
+app.use("/api/auth", authRoutes);
+
+export default app;
