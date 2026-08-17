49d753a003813ea4e3b7f6a166ee2bb0a817e5f3
hoabuiquang168-star <hoabuiquang168@gmail.com>
2026-08-17 16:46:33 +0700

    Update backend listener

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,7 @@ import playlistRoutes from "./routes/playlist.routes.js";
 import playlistSongRoutes from "./routes/playlistSong.routes.js";
 import favoriteRoutes from "./routes/favorite.routes.js";
 import historyRoutes from "./routes/history.routes.js";
+import listenerRoutes from "./routes/listener.routes.js";
 const app = express();
 
 app.use(cors());
@@ -43,4 +44,7 @@ app.use("/api/history", historyRoutes);
 
 // Song Routes
 app.use("/api/songs", songRoutes);
+
+app.use("/api/listeners", listenerRoutes);
+
 export default app;
