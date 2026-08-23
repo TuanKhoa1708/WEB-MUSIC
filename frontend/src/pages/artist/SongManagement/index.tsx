b9ee88b4ddecc457e1dc8bd09f8da28443ba135e
hoabuiquang168-star <hoabuiquang168@gmail.com>
2026-08-23 17:28:30 +0700

    Update Merge FE - BE

--- frontend/src/pages/artist/SongManagement/index.tsx
+++
@@ -1,3 +0,0 @@
-// Artists use the exact same Song Management interface as Admins.
-// The backend doesn't restrict song access by artist currently.
-export { SongManagementPage as ArtistSongManagementPage } from '@/pages/admin/SongManagement'
